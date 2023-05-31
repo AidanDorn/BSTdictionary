1a31e6ea2e3b9921663b3fd61dabf9dd84d58cd0
AidanDorn <129999851+AidanDorn@users.noreply.github.com>
2023-05-30 21:06:23 -0700

    Finalizing

---
+++ Node.java
@@ -0,0 +1,19 @@
+package BSTdictionary;
+class Node {
+  int key;
+  String firstName, lastName, address, city, state, zip, email, phoneNumber;
+  Node left, right;
+  public Node(int key, String firstName, String lastName, String address, String city, String state, String zip,
+  String email, String phoneNumber) {
+      this.key=key;
+      this.firstName=firstName;
+      this.lastName=lastName;
+      this.address=address;
+      this.city=city;
+      this.state=state;
+      this.zip=zip;
+      this.email=email;
+      this.phoneNumber=phoneNumber;
+      left=right=null;
+  }
+}
